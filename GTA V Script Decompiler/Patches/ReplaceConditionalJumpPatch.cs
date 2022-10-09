5ef2cfbcec8a7f87f7e38a50a84fddfd42d4d292
user <email@hostname>
2022-10-09 06:24:47 -0400

    Add a patch to nop conditional jumps and drop the operand

---
+++ GTA V Script Decompiler/Patches/ReplaceConditionalJumpPatch.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decompiler.Patches
+{
+    internal class ReplaceConditionalJumpPatch : Patch
+    {
+        public ReplaceConditionalJumpPatch(Function function) : base(function)
+        {
+        }
+
+        public override string GetName(int start, int end)
+        {
+            return $"Replace Conditional Jump With DROP";
+        }
+
+        public override byte[] GetPatch(int start, int end)
+        {
+            List<byte> bytes = new();
+
+            for (int i = start; i < end; i++)
+            {
+                bytes.Add((byte)Opcode.DROP);
+
+                foreach (var _ in Function.Instructions[i].Operands)
+                {
+                    bytes.Add(0);
+                }
+            }
+
+            return bytes.ToArray();
+        }
+
+        public override bool ShouldEnablePatch(int start, int end)
+        {
+            return true;
+        }
+
+        public override bool ShouldShowPatch(int start, int end)
+        {
+            return Function.Instructions[start].IsConditionJump;
+        }
+    }
+}
