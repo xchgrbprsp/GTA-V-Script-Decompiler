9ad7dd022d85be653ffdb38d9394bb92dc606435
user <email@hostname>
2022-10-17 18:57:33 -0400

    Remove unneeded files

--- GTA V Script Decompiler/Resources/Entities_combine_and_compress.bat
+++
@@ -1,3 +0,0 @@
-clang Entities_combine.cpp -o Entities_combine.exe -O3 -march=native
-Entities_combine.exe
-php Entities_compress.php
--- GTA V Script Decompiler/Resources/Entities_compress.php
+++
@@ -1,3 +0,0 @@
-<?php
-ini_set("memory_limit", "256M");
-file_put_contents("Entities.dat", gzdeflate(file_get_contents("Entities.txt")));
